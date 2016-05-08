9b5bab7cf03cf27f1a5146610dfb96ea94696e17
Carlos Mayo <carlos.mayo@gmail.com>
2016-05-08 22:59:57 +0200

    Added front page

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    //$('h1').delay(1500).addClass('animated fadeInDown');
+    setTimeout(function(){$('h1').addClass('animated fadeInDown')},500);
+    setTimeout(function(){$('#connect,p').addClass('animated fadeIn')},500);
+
+})
